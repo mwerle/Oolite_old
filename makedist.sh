6b90b620fda217a2fde79f48b99331c7b33fc21b
Dylan Smith <winston@users.berlios.de>
2005-04-28 22:14:07 +0000

    update source packaging script and README files

--- makedist.sh
+++ makedist.sh
@@ -6,7 +6,29 @@
 #
 rm -rf ~/oolite-snapshot
 mkdir ~/oolite-snapshot
-cp *.m *.c *.h *.png *.plist *.dat *.aiff GNU* README.TXT ~/oolite-snapshot
+cp *.m *.c *.h GNU* README.TXT PORTING.TXT ~/oolite-snapshot
+
+# we don't cp -r because then we'd get all the .svn stuff
+# so do it a dir at a time and copy specific files
+SRCDIR=Resources
+RESDIR=~/oolite-snapshot/Resources
+mkdir $RESDIR
+mkdir $RESDIR/AIs
+cp $SRCDIR/AIs/*.plist $RESDIR/AIs
+mkdir $RESDIR/Config
+cp $SRCDIR/Config/*.plist $RESDIR/Config
+mkdir $RESDIR/Images
+cp $SRCDIR/Images/*.png $RESDIR/Images
+mkdir $RESDIR/Models
+cp $SRCDIR/Models/*.dat $RESDIR/Models
+mkdir $RESDIR/Music
+# TODO: add music
+mkdir $RESDIR/Sounds
+cp $SRCDIR/Sounds/*.aiff $RESDIR/Sounds
+mkdir $RESDIR/Textures
+cp $SRCDIR/Textures/*.png $RESDIR/Textures
+cp $SRCDIR/Info-Oolite.plist $RESDIR
+
 cd ~/
 tar zcvf oolite-snapshot.tar.gz oolite-snapshot
 
