8459a71b5e413b82b852d7e1fac1a7e71e6200ea
Dylan Smith <winston@users.berlios.de>
2005-10-13 21:50:13 +0000

    Make tarball installer consistent with the Autopackage installer

--- makebinpkg.sh
+++ makebinpkg.sh
@@ -11,10 +11,10 @@ mkdir -p $HOME/oolite-installer
 tar cvf ~/oolite-installer/oolite-app.tar oolite.app --exclude .svn
 
 cd SelfContainedInstaller
-cp install oolite-update README.TXT PLAYING.TXT FAQ.TXT LICENSE.TXT oolite ~/oolite-installer
+cp install oolite-update.src README.TXT PLAYING.TXT FAQ.TXT LICENSE.TXT oolite.src ~/oolite-installer
 tar cvf ~/oolite-installer/oolite-deps.tar oolite-deps --exclude .svn
 echo $1 >~/oolite-installer/release.txt
 
 cd ~/
-tar zcvf oolite-x86-installer-$1.tar.gz oolite-installer
+tar zcvf oolite-$1.x86.tar.gz oolite-installer
 
