d123d4e6665a06b2feeefbbb935f8500d6381a10
Jens Ayton <ahruman@users.berlios.de>
2008-02-24 21:07:20 +0000

    Global JavaScript methods (which aren't constructors) now have names starting with lowercase letters, in accordance with JavaScript norms. There are compatibility aliases for the old forms. Log() and LogWithClass() merged into single log(). All JavaScript compatibility aliases for deprecated forms are now implemented in JavaScript, in the new oolite-global-prefix.js script. Changed all copyright notices regarding the project as a whole from 2004-2007 to 2004-2008.

--- deps/Cocoa-deps/SCRDynamicShim/SCRDynamicShim.m
+++ deps/Cocoa-deps/SCRDynamicShim/SCRDynamicShim.m
@@ -8,7 +8,7 @@ SCRDynamicShim.dylib. This is required on PPC only because Oolite uses the
 
 
 Oolite
-Copyright (C) 2004-2007 Giles C Williams and contributors
+Copyright (C) 2004-2008 Giles C Williams and contributors
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
--- tools/simpleDebugConsole/Basic-debug.oxp/Scripts/oolite-debug-console.js
+++ tools/simpleDebugConsole/Basic-debug.oxp/Scripts/oolite-debug-console.js
@@ -12,7 +12,7 @@ script, they have access to any functions in this script. You can therefore
 add debugger commands using a customized version of this script.
 
 The following properties are predefined for the script object:
-console: the console object.
+	console: the console object.
 
 The console object has the following methods:
 
@@ -26,7 +26,7 @@ function clearConsole() : void
 
 Oolite Debug OXP
 
-Copyright © 2007 Jens Ayton
+Copyright © 2007-2008 Jens Ayton
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -48,8 +48,11 @@ SOFTWARE.
 
 */
 
-this.name = "oolite-debug-console"
-this.version = "1.71"
+this.name			= "oolite-debug-console";
+this.author			= "Jens Ayton";
+this.copyright		= "© 2007-2008 the Oolite team.";
+this.description	= "Debug console script.";
+this.version		= "1.71";
 
 
 // **** Macros
@@ -60,7 +63,7 @@ this.macros =
 	setM:		"setMacro(PARAM)",
 	delM:		"deleteMacro(PARAM)",
 	showM:		"showMacro(PARAM)"
-}
+};
 
 
 // ****  Convenience functions -- copy this script and add your own here.
@@ -68,10 +71,10 @@ this.macros =
 // List the enumerable properties of an object.
 this.dumpObjectShort = function (x)
 {
-	ConsoleMessage("dumpObject", x.toString() + ":")
+	consoleMessage("dumpObject", x.toString() + ":");
 	for (let prop in x)
 	{
-		ConsoleMessage("dumpObject", "    " + prop)
+		consoleMessage("dumpObject", "    " + prop);
 	}
 }
 
@@ -79,10 +82,10 @@ this.dumpObjectShort = function (x)
 // List the enumerable properties of an object, and their values.
 this.dumpObjectLong = function (x)
 {
-	ConsoleMessage("dumpObject", x.toString() + ":")
+	consoleMessage("dumpObject", x.toString() + ":");
 	for (let prop in x)
 	{
-		ConsoleMessage("dumpObject", "    " + prop + " = " + x[prop])
+		consoleMessage("dumpObject", "    " + prop + " = " + x[prop]);
 	}
 }
 
@@ -90,12 +93,12 @@ this.dumpObjectLong = function (x)
 // Print the objects in a list on lines.
 this.printList = function (l)
 {
-	let length = l.length
+	let length = l.length;
 	
-	ConsoleMessage("printList", length.toString() + " items:")
+	consoleMessage("printList", length.toString() + " items:");
 	for (let i = 0; i != length; i++)
 	{
-		ConsoleMessage("printList", "  " + l[i].toString())
+		consoleMessage("printList", "  " + l[i].toString());
 	}
 }
 
@@ -103,17 +106,16 @@ this.printList = function (l)
 this.setColorFromString = function (string, typeName)
 { 
 	// Slice of the first component, where components are separated by one or more spaces.
-	let [key, value] = string.getOneToken()
-	
-	let fullKey = key + "-" + typeName + "-color"
+	let [key, value] = string.getOneToken();
+	let fullKey = key + "-" + typeName + "-color";
 	
 	/*	Set the colour. The "let c" stuff is so that JS property lists (like
-{ hue: 240, saturation: 0.12 } will work -- this syntax is only valid
-	in assignments.
+		{ hue: 240, saturation: 0.12 } will work -- this syntax is only valid
+		in assignments.
 	*/
-	debugConsole.settings[fullKey] = eval("let c=" + value + ";c")
+	debugConsole.settings[fullKey] = eval("let c=" + value + ";c");
 	
-	ConsoleMessage("command-result", "Set " + typeName + " colour “" + key + "” to " + value + ".")
+	consoleMessage("command-result", "Set " + typeName + " colour “" + key + "” to " + value + ".");
 }
 
 
@@ -123,32 +125,32 @@ this.consolePerformJSCommand = function (command)
 {
 	while (command.charAt(0) == " ")
 	{
-		command = command.substring(1)
+		command = command.substring(1);
 	}
 	
 	// Echo input to console, emphasising the command itself.
-	ConsoleMessage("command", "> " + command, 2, command.length)
+	consoleMessage("command", "> " + command, 2, command.length);
 	
 	if (command.charAt(0) != ":")
 	{
 		// No colon prefix, just JavaScript code.
-		this.evaluate(command, "command")
+		this.evaluate(command, "command");
 	}
 	else
 	{
 		// Colon prefix, this is a macro.
-		this.performMacro(command)
+		this.performMacro(command);
 	}
 }
 
 
 this.evaluate = function (command, type, PARAM)
 {
-	let result = eval(command)
-	if (result === null)  result = "null"
+	let result = eval(command);
+	if (result === null)  result = "null";
 	if (result !== undefined)
 	{
-		ConsoleMessage("command-result", result.toString())
+		consoleMessage("command-result", result.toString());
 	}
 }
 
@@ -160,18 +162,18 @@ this.setMacro = function (parameters)
 	if (!parameters)  return;
 	
 	// Split at first series of spaces
-	let [name, body] = parameters.getOneToken()
+	let [name, body] = parameters.getOneToken();
 	
 	if (body)
 	{
-		macros[name] = body
-		debugConsole.settings["macros"] = macros
+		macros[name] = body;
+		debugConsole.settings["macros"] = macros;
 		
-		ConsoleMessage("macro-info", "Set macro :" + name + ".")
+		consoleMessage("macro-info", "Set macro :" + name + ".");
 	}
 	else
 	{
-		ConsoleMessage("macro-error", "setMacro(): a macro definition must have a name and a body.")
+		consoleMessage("macro-error", "setMacro(): a macro definition must have a name and a body.");
 	}
 }
 
@@ -180,20 +182,20 @@ this.deleteMacro = function (parameters)
 {
 	if (!parameters)  return;
 	
-	let [name, ] = parameters.getOneToken()
+	let [name, ] = parameters.getOneToken();
 	
-	if (name.charAt(0) == ":" && name != ":")  name = name.substring(1)
+	if (name.charAt(0) == ":" && name != ":")  name = name.substring(1);
 	
 	if (macros[name])
 	{
-		delete macros[name]
-		debugConsole.settings["macros"] = macros
+		delete macros[name];
+		debugConsole.settings["macros"] = macros;
 		
-		ConsoleMessage("macro-info", "Deleted macro :" + name + ".")
+		consoleMessage("macro-info", "Deleted macro :" + name + ".");
 	}
 	else
 	{
-		ConsoleMessage("macro-info", "Macro :" + name + " is not defined.")
+		consoleMessage("macro-info", "Macro :" + name + " is not defined.");
 	}
 }
 
@@ -202,17 +204,17 @@ this.showMacro = function (parameters)
 {
 	if (!parameters)  return;
 	
-	let [name, ] = parameters.getOneToken()
+	let [name, ] = parameters.getOneToken();
 	
-	if (name.charAt(0) == ":" && name != ":")  name = name.substring(1)
+	if (name.charAt(0) == ":" && name != ":")  name = name.substring(1);
 	
 	if (macros[name])
 	{
-		ConsoleMessage("macro-info", ":" + name + " = " + macros[name])
+		consoleMessage("macro-info", ":" + name + " = " + macros[name]);
 	}
 	else
 	{
-		ConsoleMessage("macro-info", "Macro :" + name + " is not defined.")
+		consoleMessage("macro-info", "Macro :" + name + " is not defined.");
 	}
 }
 
@@ -222,32 +224,32 @@ this.performMacro = function (command)
 	if (!command)  return;
 	
 	// Strip the initial colon
-	command = command.substring(1)
+	command = command.substring(1);
 	
 	// Split at first series of spaces
-	let [macroName, parameters] = command.getOneToken()
+	let [macroName, parameters] = command.getOneToken();
 	if (macros[macroName] !== undefined)
 	{
-		let expansion = macros[macroName]
+		let expansion = macros[macroName];
 		
 		if (expansion)
 		{
 			// Show macro expansion.
-			let displayExpansion = expansion
+			let displayExpansion = expansion;
 			if (parameters)
 			{
 				// Substitute parameter string into display expansion, going from 'foo(PARAM)' to 'foo("parameters")'.
 				displayExpansion = displayExpansion.replace(/PARAM/g, '"' + parameters.substituteEscapeCodes() + '"');
 			}
-			ConsoleMessage("macro-expansion", "> " + displayExpansion)
+			consoleMessage("macro-expansion", "> " + displayExpansion);
 			
 			// Perform macro.
-			this.evaluate(expansion, "macro", parameters)
+			this.evaluate(expansion, "macro", parameters);
 		}
 	}
 	else
 	{
-		ConsoleMessage("unknown-macro", "Macro :" + macroName + " is not defined.")
+		consoleMessage("unknown-macro", "Macro :" + macroName + " is not defined.");
 	}
 }
 
@@ -270,22 +272,22 @@ this.performMacro = function (command)
  */
 String.prototype.getOneToken = function ()
 {
-	let matcher = /\s+/g		// Regular expression to match one or more spaces.
-	matcher.lastIndex = 0
-	let match = matcher.exec(this)
+	let matcher = /\s+/g;		// Regular expression to match one or more spaces.
+	matcher.lastIndex = 0;
+	let match = matcher.exec(this);
 	
 	if (match)
 	{
-		let token = this.substring(0, match.index)		// Text before spaces
-		let tail = this.substring(matcher.lastIndex)	// Text after spaces
+		let token = this.substring(0, match.index);		// Text before spaces
+		let tail = this.substring(matcher.lastIndex);	// Text after spaces
 		
-		if (token.length != 0)  return [token, tail]
-		else  return tail.getOneToken()  // Handle leading spaces case. This won't recurse more than once.
+		if (token.length != 0)  return [token, tail];
+		else  return tail.getOneToken();	// Handle leading spaces case. This won't recurse more than once.
 	}
 	else
 	{
 		// No spaces
-		return [this, null]
+		return [this, null];
 	}
 }
 
@@ -298,41 +300,49 @@ String.prototype.getOneToken = function ()
  */
 String.prototype.substituteEscapeCodes = function ()
 {
-	let string = this.replace(/\\/g, "\\\\")	// Convert \ to \\ -- must be first since we’ll be introducing new \s below.
+	let string = this.replace(/\\/g, "\\\\");	// Convert \ to \\ -- must be first since we’ll be introducing new \s below.
 	
-	string = string.replace(/\x08/g, "\\b")		// Backspace to \b
-	string = string.replace(/\f/g, "\\f")		// Form feed to \f
-	string = string.replace(/\n/g, "\\n")		// Newline to \n
-	string = string.replace(/\r/g, "\\r")		// Carriage return to \r
-	string = string.replace(/\t/g, "\\t")		// Horizontal tab to \t
-	string = string.replace(/\v/g, "\\v")		// Vertical ab to \v
-	string = string.replace(/\'/g, "\\\'")		// ' to \'			' (extra apostrophe because syntax colouring in Xcode sucks)
-	string = string.replace(/\"/g, "\\\"")		// " to \"
-	
-	return string
+	string = string.replace(/\x08/g, "\\b");	// Backspace to \b
+	string = string.replace(/\f/g, "\\f");		// Form feed to \f
+	string = string.replace(/\n/g, "\\n");		// Newline to \n
+	string = string.replace(/\r/g, "\\r");		// Carriage return to \r
+	string = string.replace(/\t/g, "\\t");		// Horizontal tab to \t
+	string = string.replace(/\v/g, "\\v");		// Vertical ab to \v
+	string = string.replace(/\'/g, '\\\'');		// ' to \'
+	string = string.replace(/\"/g, "\\\"");		// " to \"
+
+	return string;
 }
 
 
 // ****  Load-time set-up
 
 // Get the global object for easy reference
-this.console.global = (function (){ return this }).call()
+this.console.global = (function () { return this; } ).call();
 
 // Make console globally visible as debugConsole
-this.console.global.debugConsole = this.console
-debugConsole.script = this
+this.console.global.debugConsole = this.console;
+debugConsole.script = this;
 
-if (debugConsole.settings["macros"])  this.macros = debugConsole.settings["macros"]
+if (debugConsole.settings["macros"])  this.macros = debugConsole.settings["macros"];
 
 // As a convenience, make player, system and missionVariables available to console commands as single-letter variables:
-this.P = player
-this.S = system
-this.M = missionVariables
+this.P = player;
+this.S = system;
+this.M = missionVariables;
 
 
-// Make console.consoleMessage() globally visible as ConsoleMessage()
+// Make console.consoleMessage() globally visible
+function consoleMessage()
+{
+	// Call debugConsole.consoleMessage() with console as "this" and all the arguments passed to consoleMessage().
+	debugConsole.consoleMessage.apply(debugConsole, arguments);
+}
+
+
+// Also make console.consoleMessage() globally visible as ConsoleMessage() for backwards-compatibility. this will be removed before next stable.
 function ConsoleMessage()
 {
-	// Call debugConsole.consoleMessage() with console as "this" and all the arguments passed to ConsoleMessage().
-	debugConsole.consoleMessage.apply(debugConsole, arguments)
+	debugConsole.consoleMessage("warning", "Warning: ConsoleMessage() is deprecated. Use consoleMessage() instead.", 0, 8);
+	debugConsole.consoleMessage.apply(debugConsole, arguments);
 }
