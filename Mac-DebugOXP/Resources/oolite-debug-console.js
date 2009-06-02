33d2bb1b871036a5fd1e788d95ed2b3f92a4172f
Nikos Barkas <another_cmdr@users.berlios.de>
2009-06-02 16:06:23 +0000

    Removed debug flag DEBUG_SLOW_MODE, it is not needed now that we have TAF. Debug flag DEBUG_NO_DUST value changed from 0x800 to 0x400. Got rid of TAF magic numbers in the code.

--- Mac-DebugOXP/Resources/oolite-debug-console.js
+++ Mac-DebugOXP/Resources/oolite-debug-console.js
@@ -51,10 +51,10 @@ debugFlags
 		DEBUG_OCTREE_DRAW:		0x80
 		DEBUG_DRAW_NORMALS:		0x100
 		DEBUG_HIDE_HUD:			0x200
-		DEBUG_SLOW_MODE:		0x400
-		The current flags can be seen in Universe.h in the Oolite source code,
+		DEBUG_NO_DUST:			0x400
+		The current flags can be seen in Entity.h in the Oolite source code,
 		for instance at:
-		http://svn.berlios.de/svnroot/repos/oolite-linux/trunk/src/Core/Universe.h
+		http://svn.berlios.de/svnroot/repos/oolite-linux/trunk/src/Core/Entities/Entity.h
 	For example, to enable rendering of bounding boxes and surface normals,
 	you might use:
 		console.debugFlags ^= 0x40
