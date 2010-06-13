4fa54edd8b9cbdcfd8d5eb1b47ee050950a7c495
Jens Ayton <ahruman@users.berlios.de>
2010-06-13 14:04:49 +0000

    Finalizing Material Test Suite 1.0.

--- tests/materialTestSuite/Material Test Suite.oxp/Config/script.js
+++ tests/materialTestSuite/Material Test Suite.oxp/Config/script.js
@@ -27,7 +27,7 @@ MA 02110-1301, USA.
 
 
 this.name			= "oolite-material-test-suite";
-this.version		= "0.7";
+this.version		= "1.0";
 this.author			= "Jens Ayton";
 this.copyright		= "© 2010 the Oolite team.";
 
