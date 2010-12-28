8396e579048b7768549710a34334e791a616c6c2
Jens Ayton <ahruman@users.berlios.de>
2010-12-28 22:56:25 +0000

    Mac Oolite-newjs builds now use an Xcode project to build libjs.

--- deps/Cocoa-deps/SpiderMonkey/include/js-config.h
+++
@@ -1,7 +0,0 @@
-#if __i386__
-#include "js-config-i386.h"
-#elif __x86_64__
-#include "js-config-x86_64.h"
-#else
-#error Unsupported platform.
-#endif
--- deps/Cocoa-deps/SpiderMonkey/include/jsautocfg.h
+++
@@ -1,7 +0,0 @@
-#if __i386__
-#include "jsautocfg-i386.h"
-#elif __x86_64__
-#include "jsautocfg-x86_64.h"
-#else
-#error Unsupported platform.
-#endif
